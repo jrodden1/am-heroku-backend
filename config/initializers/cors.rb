a3a654558d4f6e3e19677adc65bd0df95350885a
Jeremiah Rodden <jrodden1@gmail.com>
2019-11-25 12:08:01 -0700

    Updated CORS file to support traffic from any subdomain of automaintainer.net

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -17,7 +17,7 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
    end
 
    allow do
-      origins 'automaintainer.net'
+      origins '*.automaintainer.net'
       # Allows frontend app to communicate with rails backend
 
    resource '*',
